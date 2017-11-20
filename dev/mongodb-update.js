651a7a479dd8ed4c64e064a69fbccc90eb77b5ea
DESKTOP-IQOTJ93\Brandon <brandon.pham@swyfti.com>
2017-11-20 10:23:46 -0800

    add update script

---
+++ dev/mongodb-update.js
@@ -0,0 +1,42 @@
+// const MongoClient = require('mongodb').MongoClient;
+// Deconstruct object  same as above
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/todosApp', (err, db) => {
+  if(err){
+    return console.log('Unable to connect to database server.');
+  }
+  console.log('Connected to MongoDB server');
+
+  // Update collection by ID
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5a0f7ae0b62ad8f4d8512c54')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5a0f70905cf0423ca8b0d639')
+  }, {
+    $set: {
+      name: 'Brandon Pham'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+
+  // db.close();
+});
