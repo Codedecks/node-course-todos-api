1d5818f3df8f4779614160ca1959e20d9932116b
DESKTOP-IQOTJ93\Brandon <brandon.pham@swyfti.com>
2017-11-21 08:49:51 -0800

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/todosApp', { useMongoClient: true });
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todosApp', { useMongoClient: true });
+mongoose.connect(mongodb://codedecks:Temp4me!!@ds147070.mlab.com:47070/mytasklist_dev', { useMongoClient: true });
 
 module.exports = {mongoose};
