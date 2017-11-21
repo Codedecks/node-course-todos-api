25fb5f3f57b08ec6d5c558f9adbb48b7fe09cecd
DESKTOP-IQOTJ93\Brandon <brandon.pham@swyfti.com>
2017-11-21 08:59:17 -0800

    fix db connection string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todosApp', { useMongoClient: true });
-mongoose.connect(mongodb://codedecks:Temp4me!!@ds147070.mlab.com:47070/mytasklist_dev', { useMongoClient: true });
+mongoose.connect('mongodb://codedecks:Temp4me!!@ds147070.mlab.com:47070/mytasklist_dev', { useMongoClient: true });
 
 module.exports = {mongoose};
